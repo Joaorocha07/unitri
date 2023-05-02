c57ef1bfbef0c32c857678f2ca700f98c42eba3c
João Victor Rocha Santos <joao.santos@unitri.edu.br>
2023-05-02 11:01:41 -0300

    Indentações alteradas de acordo com eslintrc

--- src/contexts/AuthLogin.jsx
+++ src/contexts/AuthLogin.jsx
@@ -1,37 +1,43 @@
 import { createContext, useMemo } from 'react';
 import axios from 'axios';
+import PropTypes from 'prop-types';
 
 export const AuthContext = createContext();
 
 const AuthLogin = ({ children }) => {
-    const login = async (userId, password, database) => {
-        try {
-            const formData = new FormData();
-            
-            formData.append('userId', userId);
-            formData.append('password', password);
-            formData.append('database', database);
+  const login = async (userId, password, database) => {
+    try {
+      const formData = new FormData();
 
-            axios.post('https://acl-hmg.prod.unitri.edu.br/login', formData)
-            .then((response) => {
-                console.log(response);
-            })
-            .catch((error) => {
-                console.log(error); 
-            })
-        } catch (error) {
-            console.log(error);
-            console.log("Erro no servidor!");
-        }
+      formData.append('userId', userId);
+      formData.append('password', password);
+      formData.append('database', database);
+
+      axios.post('https://acl-hmg.prod.unitri.edu.br/login', formData)
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    } catch (error) {
+      console.log(error);
+      console.log('Erro no servidor!');
     }
+  };
 
-    return (
-        <AuthContext.Provider value={useMemo(() => ({
-            login,
-        }), [])}>
-            {children}
-        </AuthContext.Provider>
-    );
-}
+  return (
+    <AuthContext.Provider value={useMemo(() => ({
+      login,
+    }), [])}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+AuthLogin.propTypes = {
+  children: PropTypes.node.isRequired,
+};
 
 export default AuthLogin;
